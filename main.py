c71ea36b57c536ed9118c94d74e28b950b066bca
DimaShest <dimashestakov0902@mail.ru>
2026-09-25 04:27:42 +0500

    Задание с созданием и получением книги

--- main.py
+++ main.py
@@ -29,12 +29,18 @@ class TaskCreate(BaseModel):
     title: str
 
 
+class Book(BaseModel):
+    book: str
+
+
 tasks: list[Task] = []
+book: str = ""
 
 
 @app.get("/tasks", response_model=list[Task])
 def get_tasks():
     """Получить список задач"""
+    print(book)
     return tasks
 
 
@@ -44,3 +50,16 @@ def create_task(payload: TaskCreate):
     task = Task(id=str(uuid4()), title=payload.title, completed=False)
     tasks.append(task)
     return task
+
+
+@app.get('/book', response_model=Book) 
+def get_book():
+    return {"book": "Любимая книга: " + book}
+    
+    
+@app.post("/book", response_model=Book, status_code=status.HTTP_201_CREATED)
+def create_book(payload: Book):
+    global book
+    book = payload.book
+    
+    return payload
